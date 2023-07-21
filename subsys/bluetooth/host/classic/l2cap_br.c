0adf410d06ed56f7eaa5dd9945a76793f08ceddb
Jonathan Rico <jonathan.rico@nordicsemi.no>
2023-07-21 10:22:32 +0200

    Bluetooth: host: go fast
    TODO: summary of the new architecture
    
    Signed-off-by: Jonathan Rico <jonathan.rico@nordicsemi.no>

--- subsys/bluetooth/host/classic/l2cap_br.c
+++ subsys/bluetooth/host/classic/l2cap_br.c
@@ -249,7 +249,9 @@ static int l2cap_br_send_cb(struct bt_conn *conn, uint16_t cid, struct net_buf *
 	hdr->len = sys_cpu_to_le16(buf->len - sizeof(*hdr));
 	hdr->cid = sys_cpu_to_le16(cid);
 
-	return bt_conn_send_cb(conn, buf, cb, user_data);
+	/* FIXME: add BR support */
+
+	return -ENOSYS;
 }
 
 /* Send the buffer and release it in case of failure.
