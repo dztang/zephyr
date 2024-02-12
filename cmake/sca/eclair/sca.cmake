b575fd52beda0502c7668df4d0670d01e62efcc0
Simone Ballarin <simone.ballarin@bugseng.com>
2024-02-12 19:46:18 +0100

    sca: Introduce SCA Kconfig
    Now the integration accepts a kconfig file for configuring the
    analysis and the generation of the reports. The path of the
    kconfig file should be provided via the variable ECLAIR_CONFIG.
    
    Without providing the kconfig file the first_analysis set will
    be used and no additional artefacts other than the database will
    be produced.
    
    Avoidable uses of getenv in the ECL files have been removed:
    now the options are provided directly in the eclair_(env|report)
    invocations in sca.cmake.
    
    reports.ecl is not supposed anymore to produce reports, now
    this task is handled directly by sca.cmake. The file has been
    renamed in db_generation.ecl as this is its only task.
    
    Signed-off-by: Simone Ballarin <simone.ballarin@bugseng.com>

--- cmake/sca/eclair/sca.cmake
+++ cmake/sca/eclair/sca.cmake
@@ -21,8 +21,68 @@ set(CXX_ALIASES "${CMAKE_CXX_COMPILER}")
 set(AS_ALIASES "${CMAKE_AS}")
 set(LD_ALIASES "${CMAKE_LINKER}")
 set(AR_ALIASES "${CMAKE_ASM_COMPILER_AR} ${CMAKE_C_COMPILER_AR} ${CMAKE_CXX_COMPILER_AR}")
-if(NOT ECLAIR_RULES_SET)
-  set(ECLAIR_RULES_SET first_analysis)
+
+set(ECLAIR_ENV_ADDITIONAL_OPTIONS "")
+set(ECLAIR_REPORT_ADDITIONAL_OPTIONS "")
+
+# Default value
+set(ECLAIR_RULES_SET first_analysis)
+
+if (ECLAIR_CONFIG)
+  import_kconfig(CONFIG_ECLAIR ${ECLAIR_CONFIG})
+  # ECLAIR env
+  if (CONFIG_ECLAIR_RULES_SET_first_analysis)
+    set(ECLAIR_RULES_SET first_analysis)
+  endif()
+  if (CONFIG_ECLAIR_RULES_SET_STU)
+    set(ECLAIR_RULES_SET STU)
+  endif()
+  if (CONFIG_ECLAIR_RULES_SET_STU_heavy)
+    set(ECLAIR_RULES_SET STU_heavy)
+  endif()
+  if (CONFIG_ECLAIR_RULES_SET_WP)
+    set(ECLAIR_RULES_SETWP )
+  endif()
+  if (CONFIG_ECLAIR_RULES_SET_std_lib)
+    set(ECLAIR_RULES_SET std_lib)
+  endif()
+  # ECLAIR report
+  if (CONFIG_ECLAIR_metrics_tab)
+    set(ECLAIR_REPORT_ADDITIONAL_OPTIONS "${ECLAIR_REPORT_ADDITIONAL_OPTIONS}" "-metrics_tab=${ECLAIR_OUTPUT_DIR}/metrics")
+  endif()
+  if (CONFIG_ECLAIR_reports_tab)
+    set(ECLAIR_REPORT_ADDITIONAL_OPTIONS "${ECLAIR_REPORT_ADDITIONAL_OPTIONS}" "-reports_tab=${ECLAIR_OUTPUT_DIR}/reports")
+  endif()
+  if (CONFIG_ECLAIR_summary_txt)
+    set(ECLAIR_REPORT_ADDITIONAL_OPTIONS "${ECLAIR_REPORT_ADDITIONAL_OPTIONS}" "-summary_txt=${ECLAIR_OUTPUT_DIR}/summary_txt")
+  endif()
+  if (CONFIG_ECLAIR_summary_doc)
+    set(ECLAIR_REPORT_ADDITIONAL_OPTIONS "${ECLAIR_REPORT_ADDITIONAL_OPTIONS}" "-summary_doc=${ECLAIR_OUTPUT_DIR}/summary_doc")
+  endif()
+  if (CONFIG_ECLAIR_summary_odt)
+    set(ECLAIR_REPORT_ADDITIONAL_OPTIONS "${ECLAIR_REPORT_ADDITIONAL_OPTIONS}" "-summary_odt=${ECLAIR_OUTPUT_DIR}/summary_odt")
+  endif()
+  if (CONFIG_ECLAIR_full_txt_areas_AREAS)
+    set(ECLAIR_REPORT_ADDITIONAL_OPTIONS "${ECLAIR_REPORT_ADDITIONAL_OPTIONS}" "-setq=report_areas,areas")
+  endif()
+  if (CONFIG_ECLAIR_full_txt_areas_FIRST_AREA)
+    set(ECLAIR_REPORT_ADDITIONAL_OPTIONS "${ECLAIR_REPORT_ADDITIONAL_OPTIONS}" "-setq=report_areas,first_area")
+  endif()
+  if (CONFIG_ECLAIR_full_txt)
+    set(ECLAIR_REPORT_ADDITIONAL_OPTIONS "${ECLAIR_REPORT_ADDITIONAL_OPTIONS}" "-full_txt=${ECLAIR_OUTPUT_DIR}/txt")
+  endif()
+  if (CONFIG_ECLAIR_full_doc_areas_AREAS)
+    set(ECLAIR_REPORT_ADDITIONAL_OPTIONS "${ECLAIR_REPORT_ADDITIONAL_OPTIONS}" "-setq=report_areas,areas")
+  endif()
+  if (CONFIG_ECLAIR_full_doc_areas_FIRST_AREA)
+    set(ECLAIR_REPORT_ADDITIONAL_OPTIONS "${ECLAIR_REPORT_ADDITIONAL_OPTIONS}" "-setq=report_areas,first_area")
+  endif()
+  if (CONFIG_ECLAIR_full_doc)
+    set(ECLAIR_REPORT_ADDITIONAL_OPTIONS "${ECLAIR_REPORT_ADDITIONAL_OPTIONS}" "-full_doc=${ECLAIR_OUTPUT_DIR}/doc")
+  endif()
+  if (CONFIG_ECLAIR_full_odt)
+    set(ECLAIR_REPORT_ADDITIONAL_OPTIONS "${ECLAIR_REPORT_ADDITIONAL_OPTIONS}" "-full_odt=${ECLAIR_OUTPUT_DIR}/odt")
+  endif()
 endif()
 
 message(STATUS "ECLAIR outputs have been written to: ${ECLAIR_OUTPUT_DIR}")
@@ -37,11 +97,8 @@ add_custom_target(eclair ALL
   COMMAND ${CMAKE_COMMAND} -E
     make_directory ${ECLAIR_DATA_DIR}
   COMMAND ${CMAKE_COMMAND} -E env
-    ECLAIR_PROJECT_NAME=${ECLAIR_PROJECT_NAME}
-    ECLAIR_PROJECT_ROOT=${ZEPHYR_BASE}
     ECLAIR_DIAGNOSTICS_OUTPUT=${ECLAIR_DIAGNOSTICS_OUTPUT}
     ECLAIR_DATA_DIR=${ECLAIR_DATA_DIR}
-    ECLAIR_RULES_SET=${ECLAIR_RULES_SET}
     CC_ALIASES=${CC_ALIASES}
     CXX_ALIASES=${CXX_ALIASES}
     AS_ALIASES=${AS_ALIASES}
@@ -50,7 +107,11 @@ add_custom_target(eclair ALL
     --
     ${ECLAIR_ENV}
     -verbose
+    -project_name=${ECLAIR_PROJECT_NAME}
+    -project_root=${ZEPHYR_BASE}
     -eval_file=${ECLAIR_ECL_DIR}/analysis.ecl
+    -eval_file=${ECLAIR_ECL_DIR}/analysis_${ECLAIR_RULES_SET}.ecl
+    ${ECLAIR_ENV_ADDITIONAL_OPTIONS}
     -- west build -p always
                   -b ${BOARD}
                   --build-dir ${ECLAIR_BUILD_DIR}
@@ -59,21 +120,13 @@ add_custom_target(eclair ALL
     ECLAIR_DATA_DIR=${ECLAIR_DATA_DIR}
     ECLAIR_OUTPUT_DIR=${ECLAIR_OUTPUT_DIR}
     ECLAIR_PROJECT_ECD=${ECLAIR_PROJECT_ECD}
-    ECLAIR_metrics_tab=${ECLAIR_metrics_tab}
-    ECLAIR_reports_tab=${ECLAIR_reports_tab}
-    ECLAIR_summary_txt=${ECLAIR_summary_txt}
-    ECLAIR_summary_doc=${ECLAIR_summary_doc}
-    ECLAIR_summary_odt=${ECLAIR_summary_odt}
-    ECLAIR_full_txt_areas=${ECLAIR_full_txt_areas}
-    ECLAIR_full_txt=${ECLAIR_full_txt}
-    ECLAIR_full_doc_areas=${ECLAIR_full_doc_areas}
-    ECLAIR_full_doc=${ECLAIR_full_doc}
-    ECLAIR_full_odt=${ECLAIR_full_odt}
     --
     ${ECLAIR_REPORT}
-    -eval_file=${ECLAIR_ECL_DIR}/reports.ecl
+    -quiet
+    -eval_file=${ECLAIR_ECL_DIR}/db_generation.ecl
   COMMAND ${ECLAIR_REPORT}
     -db=${ECLAIR_PROJECT_ECD}
+    ${ECLAIR_REPORT_ADDITIONAL_OPTIONS}
     -overall_txt=${ECLAIR_OUTPUT_DIR}/overall.txt
   COMMAND ${CMAKE_COMMAND} -E
     cat ${ECLAIR_OUTPUT_DIR}/overall.txt
@@ -82,14 +135,3 @@ add_custom_target(eclair ALL
   COMMAND_EXPAND_LISTS
 )
 
-unset(ECLAIR_RULES_SET CACHE)
-unset(ECLAIR_metrics_tab CACHE)
-unset(ECLAIR_reports_tab CACHE)
-unset(ECLAIR_summary_txt CACHE)
-unset(ECLAIR_summary_doc CACHE)
-unset(ECLAIR_summary_odt CACHE)
-unset(ECLAIR_full_txt_areas CACHE)
-unset(ECLAIR_full_txt CACHE)
-unset(ECLAIR_full_doc_areas CACHE)
-unset(ECLAIR_full_doc CACHE)
-unset(ECLAIR_full_odt CACHE)
