6a0556440a0ae710a5e994160dcf2a18c5322b8b
Daniel Schultz <d.schultz@phytec.de>
2024-04-11 16:10:08 -0700

    boards: ti: sk_am62: Add OpenOCD support
    The SK AM62 EVM is equipped with an XDS110 JTAG debugger. Add
    missing OpenOCD configuration and some documentation to support
    'west debug'.
    
    Signed-off-by: Daniel Schultz <d.schultz@phytec.de>

---
+++ boards/ti/sk_am62/board.cmake
@@ -0,0 +1,13 @@
+# Texas Instruments Sitara AM62x-SK-M4 EVM
+#
+# Copyright (c) 2024, PHYTEC Messtechnik GmbH
+#
+# SPDX-License-Identifier: Apache-2.0
+
+if(CONFIG_SOC_AM6234_M4)
+  board_set_debugger_ifnset(openocd)
+  board_set_flasher_ifnset(openocd)
+
+  board_runner_args(openocd "--no-init" "--no-halt" "--no-targets" "--gdb-client-port=3339")
+  include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+endif()
